9e7c61cd2c06ef507f2b89209b90eddef797c959
PhePheMath <phelyppemath@outlook.com>
2021-12-13 22:34:31 -0300

    Componente de Tela de Bem Vindo

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { SettingOutlined } from '@ant-design/icons'
 import styles from './style.module.scss'
+import LoginForm from '../../components/LoginForm';
 
 export default class Login extends Component {
+
     render() {
         return (
             <div className={styles.Center}>
@@ -11,11 +13,7 @@ export default class Login extends Component {
                     <h1 className={styles.title}>PF SERVIÇOS</h1>
                     <h4 className={styles.subtitle}>OFICINA MECÂNICA</h4>
 
-                    <input className={styles.username} type="text" placeholder='Username' />
-                    <input className={styles.password} type="text" placeholder='Password' />
-
-                    <input type="button" className={styles.retrieve} value='Esqueci a senha' />
-                    <input type="button" className={styles.login} value='LOGIN' />
+                    <LoginForm />
                 </div>
             </div>
         )
