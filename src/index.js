9b1ee04e672470ff1fea3d3ae56c160a66ebf5a8
PhePheMath <phelyppemath@outlook.com>
2021-12-13 22:32:22 -0300

    Criando rotas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
+import './style.module.scss'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path='/services' element={<Home />} />
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
